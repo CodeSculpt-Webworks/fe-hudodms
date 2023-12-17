ff0a55d8a7ef8907686322b4daabfe20f78ed1fa
JeikuDevvv <jakelourencevillar.august@gmail.com>
2023-12-18 04:36:31 +0800

    stuff

--- src/components/container/Incoming_outgoing/IOform.tsx
+++ src/components/container/Incoming_outgoing/IOform.tsx
@@ -1,7 +1,9 @@
 import "./IOform.css";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { Button, TextInput, RadioButton } from "../..";
+import { HudoLogo } from "../../../assets";
 import { useDropzone } from "react-dropzone";
+import Modal from "react-modal";
 
 const inputStyle = {
   height: 55,
@@ -24,42 +26,123 @@ const buttonStyle = {
   marginRight: 10,
 };
 
-export const IOform = () => {
-  const { getRootProps, getInputProps } = useDropzone();
+Modal.setAppElement("#root");
 
-  const [selectedOption, setSelectedOption] = useState("");
+export const IOform = () => {
+  const [formData, setFormData] = useState({
+    title: "",
+    trackingNumber: "",
+    from: "",
+    to: "",
+    subject: "",
+    option: "",
+    attachmentName: "",
+  });
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop: useCallback(
+      (acceptedFiles: File[]) => {
+        const attachmentName =
+          acceptedFiles.length > 0 ? acceptedFiles[0].name : "";
+        setFormData({
+          ...formData,
+          attachmentName,
+        });
+
+        onFileUpload(acceptedFiles);
+      },
+      [formData]
+    ),
+  });
 
   const handleRadioChange = (value: string) => {
-    setSelectedOption(value);
+    setFormData({ ...formData, option: value });
+  };
+
+  const handleInputChange = (key: string, value: string) => {
+    setFormData({ ...formData, [key]: value });
+  };
+
+  const handleSubmit = () => {
+    if (Object.values(formData).some((value) => value === "")) {
+      setModalIsOpen(true);
+    } else {
+      console.log("Form Data:", formData);
+    }
+  };
+
+  const handleClear = () => {
+    setFormData({
+      title: "",
+      trackingNumber: "",
+      from: "",
+      to: "",
+      subject: "",
+      option: "",
+      attachmentName: "",
+    });
+  };
+
+  const onFileUpload = (files: File[]) => {
+    console.log("Uploaded Files:", files);
   };
 
   return (
     <div className="main-container">
       <div className="form-container">
         <div className="row1">
-          <TextInput label="Title" style={fullWidthInputStyle} />
-          <TextInput label="Tracking Number" style={halfWidthInputStyle} />
+          <TextInput
+            label="Title"
+            style={fullWidthInputStyle}
+            value={formData.title}
+            onChange={(e) => handleInputChange("title", e.target.value)}
+          />
+          <TextInput
+            label="Tracking Number"
+            style={halfWidthInputStyle}
+            value={formData.trackingNumber}
+            onChange={(e) =>
+              handleInputChange("trackingNumber", e.target.value)
+            }
+          />
         </div>
         <div className="row2">
-          <TextInput label="From" style={halfWidthInputStyle} />
-          <TextInput label="To" style={halfWidthInputStyle} />
+          <TextInput
+            label="From"
+            style={halfWidthInputStyle}
+            value={formData.from}
+            onChange={(e) => handleInputChange("from", e.target.value)}
+          />
+          <TextInput
+            label="To"
+            style={halfWidthInputStyle}
+            value={formData.to}
+            onChange={(e) => handleInputChange("to", e.target.value)}
+          />
         </div>
         <div className="row3">
-          <TextInput label="Subject" style={halfWidthInputStyle} />
+          <TextInput
+            label="Subject"
+            style={halfWidthInputStyle}
+            value={formData.subject}
+            onChange={(e) => handleInputChange("subject", e.target.value)}
+          />
           <div className="sub3">
             <RadioButton
               label="Incoming"
               value="incoming"
-              checked={selectedOption === "incoming"}
-              onChange={handleRadioChange}
+              checked={formData.option === "incoming"}
+              onChange={() => handleRadioChange("incoming")}
               style={halfWidthInputStyle}
             />
             <RadioButton
               label="Outgoing"
               value="outgoing"
-              checked={selectedOption === "outgoing"}
+              checked={formData.option === "outgoing"}
+              onChange={() => handleRadioChange("outgoing")}
               style={halfWidthInputStyle}
-              onChange={handleRadioChange}
             />
           </div>
         </div>
@@ -76,14 +159,32 @@ export const IOform = () => {
             marginBottom: "20px",
           }}
         >
-          <input {...getInputProps()} />
+          <input
+            {...getInputProps()}
+            onChange={(e) =>
+              handleInputChange("attachmentName", e.target.value)
+            }
+          />
           <p>Drag 'n' drop some files here, or click to select files</p>
         </div>
       </div>
       <div className="button-container">
-        <Button label="Clear" style={buttonStyle} />
-        <Button label="Submit" style={buttonStyle} />
+        <Button label="Clear" style={buttonStyle} onClick={handleClear} />
+        <Button label="Submit" style={buttonStyle} onClick={handleSubmit} />
       </div>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        contentLabel="Example Modal"
+        className="modal-container"
+      >
+        <img src={HudoLogo} alt="hudo-logo" />
+        <p>
+          You forgot to fill in all the fields. Please fill in all the fields.
+        </p>
+        <button onClick={() => setModalIsOpen(false)}>Close</button>
+      </Modal>
     </div>
   );
 };
